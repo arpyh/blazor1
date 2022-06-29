453a4d26b22da32fb29f8db06afdac4286c53ff7
Riste Poposki <riste.poposki.haselt@outlook.com>
2022-06-29 16:50:55 +0200

    Using HttpClientFactory to get data from API and bind to Blazor

--- BlazorApp1/BlazorApp1/Program.cs
+++ BlazorApp1/BlazorApp1/Program.cs
@@ -1,3 +1,4 @@
+using BethanysPieShopHRM.App.Services;
 using BlazorApp1;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -6,6 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient<IEmployeeDataService, EmployeeDataService>(client => client.BaseAddress = new Uri("https://localhost:44340/"));
+
 
 await builder.Build().RunAsync();
---
+++ BlazorApp1/BlazorApp1/Services/EmployeeDataService.cs
@@ -0,0 +1,35 @@
+﻿using BethanysPieShopHRM.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.App.Services
+{
+    public class EmployeeDataService : IEmployeeDataService
+    {
+        private readonly HttpClient _httpClient;
+
+        public EmployeeDataService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<Employee>> GetAllEmployees()
+        {
+            return await _httpClient.GetFromJsonAsync<IEnumerable<Employee>>($"api/employee");
+        }
+
+        public async Task<Employee> GetEmployeeDetails(int employeeId)
+        {
+            return await JsonSerializer.DeserializeAsync<Employee>
+                (await _httpClient.GetStreamAsync($"api/employee/{employeeId}"), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        }
+
+
+    }
+}
