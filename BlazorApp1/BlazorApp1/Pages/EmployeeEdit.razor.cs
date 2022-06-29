cf1caea0f5f37a8a8b5002e3dbf74535b295e2b0
Riste Poposki <riste.poposki.haselt@outlook.com>
2022-06-29 17:20:20 +0200

    simple edit form

---
+++ BlazorApp1/BlazorApp1/Pages/EmployeeEdit.razor.cs
@@ -0,0 +1,22 @@
+﻿using BethanysPieShopHRM.App.Services;
+using BethanysPieShopHRM.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp1.Pages
+{
+    public partial class EmployeeEdit
+    {
+        [Inject]
+        private IEmployeeDataService _employeeDataService { get; set; }
+
+        [Parameter]
+        public string EmployeeId { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employee = await _employeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+        }
+    }
+}
